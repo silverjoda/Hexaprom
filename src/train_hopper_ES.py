7d2fc73fdba56af2c814c8af84719a48dadfb279
silverjoda <heizen.sh@gmail.com>
2018-08-01 15:46:53 +0200

    Initcom

--- src/train_hopper_ES.py
+++ src/train_hopper_ES.py
@@ -1,5 +1,6 @@
 import numpy as np
 import gym
+import cma
 np.random.seed(0)
 
 def make_nn(w):
@@ -22,16 +23,16 @@ def f(w):
         # Make actions:
 
         # Oscillator 0
-        o0 = env_obs[0:4] + [prev_torques[0]]
-        a0, m0 = np.matmul(o0, w[0:n1])
+        o0 = list(env_obs[0:4]) + prev_torques[0:1]
+        a0, m0 = np.maximum(np.matmul(o0, w[0:n1].reshape((5,2))), 0)
 
         # Oscillator 1
-        o1 = env_obs[2:5] + [prev_torques[1]]
-        a1, m1 = np.matmul(o1, w[n1:n1 + n2])
+        o1 = list(env_obs[2:5]) + prev_torques[1:2]
+        a1, m1 = np.maximum(np.matmul(o1, w[n1:n1 + n2].reshape((4,2))), 0)
 
         # Oscillator 2
-        o2 = env_obs[3:5] + [prev_torques[2]]
-        a2, m2 = np.matmul(o2, w[n1 + n2:])
+        o2 = list(env_obs[3:5]) + prev_torques[2:3]
+        a2, m2 = np.maximum(np.matmul(o2, w[n1 + n2:].reshape((3,2))), 0)
 
         t0 = a0 * np.sin(m0 * step)
         t1 = a1 * np.sin(m1 * step)
@@ -44,18 +45,40 @@ def f(w):
             env.render()
 
         reward += rew
-        step += 0.01
+        step += 0.05
 
         prev_torques = [t0, t1, t2]
 
-    return reward
+    return -reward
+
+def train_NES(w, N_iters):
+    for i in range(N_iters):
+
+        # initialize memory for a population of w's, and their rewards
+        N = np.random.randn(npop, N_weights)  # samples from a normal distribution N(0,1)
+        R = np.zeros(npop)
+        for j in range(npop):
+            w_try = w + sigma * N[j]  # jitter w using gaussian of sigma 0.1
+            R[j] = f(w_try)  # evaluate the jittered version
+
+            # standardize the rewards to have a gaussian distribution
+            A = (R - np.mean(R)) / np.std(R)
+            # perform the parameter update. The matrix multiply below
+            # is just an efficient way to sum up all the rows of the noise matrix N,
+            # where each row N[j] is weighted by A[j]
+            w = w + alpha / (npop * sigma) * np.dot(N.T, A)
+
+        # print current fitness of the most likely parameter setting
+        if i % 10 == 0:
+            print('iter %d. Reward: %f' %
+                  (i, f(w)))
 
 # hyperparameters
 npop = 50 # population size
-sigma = 0.1 # noise standard deviation
+sigma = 0.5 # noise standard deviation
 alpha = 0.001 # learning rate
 N_iters = 1000
-animate = True
+animate = False
 
 # Make environment
 env = gym.make("Hopper-v2")
@@ -68,23 +91,9 @@ n3 = (3 * 2)
 N_weights = n1 + n2 + n3
 w = np.random.randn(N_weights)
 
-for i in range(N_iters):
+#train_NES(w, N_iters)
 
-    # initialize memory for a population of w's, and their rewards
-    N = np.random.randn(npop, N_weights) # samples from a normal distribution N(0,1)
-    R = np.zeros(npop)
-    for j in range(npop):
-        w_try = w + sigma * N[j] # jitter w using gaussian of sigma 0.1
-        R[j] = f(w_try) # evaluate the jittered version
+es = cma.CMAEvolutionStrategy(w, 0.5)
+es.optimize(f, iterations=None)
+es.result_pretty()
 
-        # standardize the rewards to have a gaussian distribution
-        A = (R - np.mean(R)) / np.std(R)
-        # perform the parameter update. The matrix multiply below
-        # is just an efficient way to sum up all the rows of the noise matrix N,
-        # where each row N[j] is weighted by A[j]
-        w = w + alpha / (npop * sigma) * np.dot(N.T, A)
-
-    # print current fitness of the most likely parameter setting
-    if i % 10 == 0:
-        print('iter %d. Reward: %f' %
-              (i, f(w)))
