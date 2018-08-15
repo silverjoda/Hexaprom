51de578499ea392f9fdb324bb042005f0a1ac16e
silverjoda <heizen.sh@gmail.com>
2018-08-15 14:46:07 +0200

    Initcom

--- src/train_ant_ES_EXP.py
+++ src/train_ant_ES_EXP.py
@@ -18,42 +18,42 @@ def f(w):
 
     while not done:
 
-        # TODO: Remove global observations and add velocity and previous torques to joints
+        # TODO: !!!! Maybe probrem is mismatch of observations and legs???? !!!!
 
         # cpol
         ocpol = list(env_obs[[0, 1, 2, 3, 5, 7, 9, 11, 13, 14, 15, 16, 17, 18]])
         pcpol = np.tanh(np.matmul(ocpol, w[:n1].reshape(n1_s)))
 
         # l0f
-        o0 = list(pcpol[0:2]) + list(env_obs[[5, 6]]) #+ prev_torques[0:2]
-        t0 = mult * np.tanh(np.matmul(o0, w[n1:n1 + 4].reshape(n2_s)))
+        o0 = list(pcpol[0:2]) + list(env_obs[[5, 6, 19, 20]]) + list(np.array(prev_torques)[[0,2,4,6]])
+        t0 = mult * np.tanh(np.matmul(o0, w[n1:n1 + 10].reshape(n2_s)))
 
         # l1f
-        o2 = list(pcpol[2:4]) + list(env_obs[[7, 8]]) #+ prev_torques[2:4]
-        t2 = mult * np.tanh(np.matmul(o2, w[n1 + 4:n1 + 8].reshape(n2_s)))
+        o2 = list(pcpol[2:4]) + list(env_obs[[7, 8, 21, 22]]) + list(np.array(prev_torques)[[0,2,4,6]])
+        t2 = mult * np.tanh(np.matmul(o2, w[n1 + 10:n1 + 20].reshape(n2_s)))
 
         # l2f
-        o4 = list(pcpol[4:6]) + list(env_obs[[9, 10]]) #+ prev_torques[4:6]
-        t4 = mult * np.tanh(np.matmul(o4, w[n1 + 8:n1 + 12].reshape(n2_s)))
+        o4 = list(pcpol[4:6]) + list(env_obs[[9, 10, 23, 24]]) + list(np.array(prev_torques)[[0,2,4,6]])
+        t4 = mult * np.tanh(np.matmul(o4, w[n1 + 20:n1 + 30].reshape(n2_s)))
 
         # l3f
-        o6 = list(pcpol[6:8]) + list(env_obs[[11, 12]]) #+ prev_torques[6:8]
-        t6 = mult * np.tanh(np.matmul(o6, w[n1 + 12:n1 + 16].reshape(n2_s)))
+        o6 = list(pcpol[6:8]) + list(env_obs[[11, 12, 25, 26]]) + list(np.array(prev_torques)[[0,2,4,6]])
+        t6 = mult * np.tanh(np.matmul(o6, w[n1 + 30:n1 + 40].reshape(n2_s)))
 
         # l0c
-        o1 = list(env_obs[[5, 6]]) + [t0]  #+ prev_torques[1:2]
+        o1 = list(env_obs[[5, 6, 19, 20]]) + [t0] + prev_torques[1:2]
         t1 = mult * np.tanh(np.matmul(o1, w[n1 + n2:].reshape(n3_s)))
 
         # l1c
-        o3 = list(env_obs[[7, 8]]) + [t2] #+ prev_torques[3:4]
+        o3 = list(env_obs[[7, 8, 21, 22]]) + [t2] + prev_torques[3:4]
         t3 = mult * np.tanh(np.matmul(o3, w[n1 + n2:].reshape(n3_s)))
 
         # l2c
-        o5 = list(env_obs[[9, 10]]) + [t4] #+ prev_torques[5:6]
+        o5 = list(env_obs[[9, 10, 23, 24]]) + [t4] + prev_torques[5:6]
         t5 = mult * np.tanh(np.matmul(o5, w[n1 + n2:].reshape(n3_s)))
 
         # l3c
-        o7 = list(env_obs[[11, 12]]) + [t6] #+ prev_torques[7:8]
+        o7 = list(env_obs[[11, 12, 25, 26]]) + [t6] + prev_torques[7:8]
         t7 = mult * np.tanh(np.matmul(o7, w[n1 + n2:].reshape(n3_s)))
 
         action = np.array([t0, t1, t2, t3, t4, t5, t6, t7])
@@ -72,12 +72,12 @@ def f(w):
 
 # Make environment
 env = gym.make("Ant-v3")
-animate = True
+animate = False
 
 # Generate weights
 n1_s = (14, 8)
-n2_s = (4, 1)
-n3_s = (3, 1)
+n2_s = (10, 1)
+n3_s = (6, 1)
 
 n1 = n1_s[0] * n1_s[1]
 n2 = 4 * n2_s[0] * n2_s[1]
--- src/train_hopper_ES_EXP3.py
+++ src/train_hopper_ES_EXP3.py
@@ -20,18 +20,18 @@ def f(w):
         # z, th, j1, j2, j3, dx, dz, dth, dj1, dj2, dj3
 
         # Oscillator 0
-        o0 = list(env_obs[[0, 1, 2, 5, 6, 7]]) + prev_torques[0:2]
-        t0 = np.tanh(np.matmul(o0, w[0:n1_a].reshape((8, 3))))
-        t0 = mult * np.tanh(np.matmul(t0, w[n1_a:n1].reshape((3, 1))))
+        o0 = list(env_obs[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]) + prev_torques[0:2]
+        t0 = np.tanh(np.matmul(o0, w[0:n1_a].reshape((13, 3))))
+        t0, s0 = mult * np.tanh(np.matmul(t0, w[n1_a:n1].reshape((3, 2))))
 
         # Oscillator 1
-        o1 = list(env_obs[[2,3,4]]) + [t0] + prev_torques
-        t1 = np.tanh(np.matmul(o1, w[n1:n1+n2_a].reshape((7, 3))))
-        t1 = mult * np.tanh(np.matmul(t1, w[n1+n2_a:n1 + n2].reshape((3, 1))))
+        o1 = list(env_obs[[2,3,4]]) + [t0, s0] + prev_torques
+        t1 = np.tanh(np.matmul(o1, w[n1:n1+n2_a].reshape((8, 3))))
+        t1, s1 = mult * np.tanh(np.matmul(t1, w[n1+n2_a:n1 + n2].reshape((3, 2))))
 
         # Oscillator 2
-        o2 = list(env_obs[[3,4]]) + [t1] + prev_torques[1:]
-        t2 = np.tanh(np.matmul(o2, w[n1 + n2:n1 + n2 + n3_a].reshape((5, 2))))
+        o2 = list(env_obs[[3,4]]) + [t1, s1] + prev_torques[1:]
+        t2 = np.tanh(np.matmul(o2, w[n1 + n2:n1 + n2 + n3_a].reshape((6, 2))))
         t2 = mult * np.tanh(np.matmul(t2, w[n1 + n2 + n3_a:n1 + n2 + n3].reshape((2, 1))))
 
         # Step environment
@@ -50,15 +50,15 @@ env = gym.make("Hopper-v2")
 animate = False
 
 # Generate weights
-n1_a = (8 * 3)
-n1_b = (3 * 1)
+n1_a = (13 * 3)
+n1_b = (3 * 2)
 n1 = n1_a + n1_b
 
-n2_a = (7 * 3)
-n2_b = (3 * 1)
+n2_a = (8 * 3)
+n2_b = (3 * 2)
 n2 = n2_a + n2_b
 
-n3_a = (5 * 2)
+n3_a = (6 * 2)
 n3_b = (2 * 1)
 n3 = n3_a + n3_b
 
