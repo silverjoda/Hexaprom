8411ed75c3e53ab089f7f3da3c4eb8481d8951ba
silverjoda <heizen.sh@gmail.com>
2018-08-30 18:40:30 +0200

    Initcom

--- src/sin_osc_PT.py
+++ src/sin_osc_PT.py
@@ -1,80 +1,8 @@
 import numpy as np
 import torch
-import matplotlib.pyplot as plt
 import torch.nn as nn
-import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
-def f(w, plot=False):
-
-    # Make frequency pattern
-    N = 40
-    F1 = 0.9
-    F2 = 0.4
-    sig = np.sin(F1 * np.arange(N)) + np.sin(F2 * np.arange(N))
-    pattern = np.fft.rfft(sig)
-    real = np.abs(pattern.real)
-    imag = pattern.imag
-
-    out1, out2, state1, state2 = [0,0,0,0]
-
-    outputs = np.zeros((3, N))
-    states = np.zeros((2, N))
-
-    for i in range(N):
-
-        # fl
-        o1, s1  = np.matmul(np.array([out1, out2, state1]), wdist.get_w('w1', w)) + wdist.get_w('b1', w)
-        o2, s2 = np.matmul(np.array([out2, out1, state2]), wdist.get_w('w2', w)) + wdist.get_w('b2', w)
-
-        out1 = o1; out2 = o2; state1 = s1; state2 = s2; out = out1 + out2
-
-        # Step environment
-        outputs[:, i] = [out1, out2, out]
-        states[:, i] = [s1, s2]
-
-    pred_pattern = np.fft.rfft(outputs[2,:])
-    pred_real = np.abs(pred_pattern.real)
-    pred_imag = pred_pattern.imag
-
-    loss = np.mean(np.square(pred_real - real)) + np.mean(np.square(pred_imag - imag))
-    #loss = np.mean(np.square(np.array(outputs[2,:]) - sig))
-
-    if plot:
-        plt.figure(1)
-        plt.subplot(711)
-        plt.ylabel('sig')
-        plt.plot(np.arange(N), sig, 'k')
-
-        plt.subplot(712)
-        plt.ylabel('sig_real')
-        plt.plot(np.arange(N / 2 + 1), real, 'b')
-
-        plt.subplot(713)
-        plt.ylabel('sig_imag')
-        plt.plot(np.arange(N / 2 + 1), imag, 'b')
-
-        plt.subplot(714)
-        plt.ylabel('predicted_sig')
-        plt.plot(np.arange(N), outputs[0,:], 'r')
-        plt.plot(np.arange(N), outputs[1,:], 'g')
-        plt.plot(np.arange(N), outputs[2,:], 'k')
-
-        plt.subplot(715)
-        plt.ylabel('pred_real')
-        plt.plot(np.arange(N/2 + 1), pred_real, 'r')
-
-        plt.subplot(716)
-        plt.ylabel('pred_imag')
-        plt.plot(np.arange(N / 2 + 1), pred_imag, 'r')
-
-        plt.subplot(717)
-        plt.ylabel('states')
-        plt.plot(np.arange(N), states[0, :], 'r')
-        plt.plot(np.arange(N), states[1, :], 'g')
-
-        plt.show()
-
-    return loss
 
 class Net(nn.Module):
 
@@ -83,9 +11,7 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(2, 2)
 
     def forward(self, x):
-        # Max pooling over a (2, 2) window
-        self.fc1(x)
-        return x
+        return self.fc1(x)
 
     def num_flat_features(self, x):
         size = x.size()[1:]  # all dimensions except the batch dimension
@@ -94,19 +20,51 @@ class Net(nn.Module):
             num_features *= s
         return num_features
 
-def matchsignal(net, iters, N):
+def matchsignal(net, iters, sig):
 
-    # Make signal
-    F1 = 0.9
-    F2 = 0.4
-    sig = np.sin(F1 * np.arange(N))
-    pattern = np.fft.rfft(sig)
-    real = np.abs(pattern.real)
-    imag = pattern.imag
+    N = len(sig)
 
     # Torch objects
     lossfun = nn.MSELoss()
-    optimizer = torch.optim.Adam(net.parameters(), lr=0.005)
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
+
+    # Train iters times
+    for i in range(iters):
+
+        # Initial values
+        s = torch.tensor(0., requires_grad=True)
+        out = torch.tensor(0., requires_grad=True)
+
+        outputs = []
+        states = []
+
+        # Single iteration of signal generation
+        for j in range(N):
+            x = torch.tensor([s, out], dtype=torch.float32)
+            out, s = net(x)
+            outputs.append(out)
+            states.append(s)
+
+        # Zero the gradient buffers and calculate loss
+        optimizer.zero_grad()
+        loss = lossfun(torch.stack(outputs), torch.from_numpy(sig).float())
+
+        # Backprop
+        loss.backward()
+
+        # Apply gradients
+        optimizer.step()
+
+        if i % 50 == 0:
+            print("Iter {}/{}, loss: {}".format(i, iters, loss))
+
+
+def eval(net, sig):
+
+    N = len(sig)
+    pattern = np.fft.rfft(sig)
+    real = np.abs(pattern.real)
+    imag = pattern.imag
 
     # Initial values
     s = 0
@@ -115,44 +73,67 @@ def matchsignal(net, iters, N):
     outputs = []
     states = []
 
-    # Train iters times
-    for i in range(iters):
+    for j in range(N):
+        x = torch.tensor([s, out], dtype=torch.float32)
+        out, s = net(x)
+        outputs.append(out.item())
+        states.append(s.item())
 
-        # Single iteration of signal generation
-        for j in range(N):
-            out, s = net(np.expand_dims([s,out], axis=0))
-            outputs.append(out)
-            states.append(s)
+    pattern_pred = np.fft.rfft(np.array(outputs))
+    pred_real = np.abs(pattern_pred.real)
+    pred_imag = pattern_pred.imag
 
-        # Zero the gradient buffers and calculate loss
-        optimizer.zero_grad()
-        loss = lossfun(outputs, sig)
+    plt.figure(1)
+    plt.subplot(711)
+    plt.ylabel('sig')
+    plt.plot(np.arange(N), sig, 'k')
 
-        # Backprop
-        loss.backwards()
+    plt.subplot(712)
+    plt.ylabel('sig_real')
+    plt.plot(np.arange(N / 2 + 1), real, 'b')
 
-        # Apply gradients
-        optimizer.step()
+    plt.subplot(713)
+    plt.ylabel('sig_imag')
+    plt.plot(np.arange(N / 2 + 1), imag, 'b')
 
-        if iters % 10 == 0:
-            print("Iter {}/{}, loss: {}".format(i, iters, loss))
+    plt.subplot(714)
+    plt.ylabel('predicted_sig')
+    plt.plot(np.arange(N), outputs, 'r')
 
 
-def eval(net, N):
-    pass
+    plt.subplot(715)
+    plt.ylabel('pred_real')
+    plt.plot(np.arange(N/2 + 1), pred_real, 'k')
+
+    plt.subplot(716)
+    plt.ylabel('pred_imag')
+    plt.plot(np.arange(N / 2 + 1), pred_imag, 'r')
+
+    plt.subplot(717)
+    plt.ylabel('states')
+    plt.plot(np.arange(N), states, 'r')
+
+    plt.show()
+
 
 # Define parameters
-N = 50
+N = 30
 ITERS = 1000
 
 # Make oscillator model
 osc = Net()
+print(osc)
+
+# Make signal
+F1 = 0.9
+F2 = 0.4
+sig = np.sin(F1 * np.arange(N))
 
 # Train
 try:
-    matchsignal(osc, ITERS, N)
+    matchsignal(osc, ITERS, sig)
 except KeyboardInterrupt:
     print("User interrupted process.")
 
 #Evaluate result
-eval(osc, N)
+eval(osc, sig)
