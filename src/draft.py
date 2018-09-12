941c45f876dfcad854e8928bbcd5f4e026194b46
silverjoda <heizen.sh@gmail.com>
2018-09-12 15:35:53 +0200

    Initcom

--- src/draft.py
+++ src/draft.py
@@ -1,163 +1,17 @@
-import argparse
-from collections import deque
-import copy
-from functools import partial
-import gc
-import logging
-from multiprocessing.pool import ThreadPool
-import os
-import pickle
-import random
-import sys
-import time
-
-# from evostra import EvolutionStrategy
-from pytorch_es import EvolutionModule
-from pytorch_es.utils.helpers import weights_init
-import gym
-from gym import logger as gym_logger
+import matplotlib.pyplot as plt
 import numpy as np
-import torch
-from torch.autograd import Variable
-import torch.nn as nn
 
-gym_logger.setLevel(logging.CRITICAL)
+x = np.linspace(0, 6*np.pi, 100)
+y = np.sin(x)
 
+# You probably won't need this if you're embedding things in a tkinter plot...
+plt.ion()
 
+fig = plt.figure()
+ax = fig.add_subplot(111)
+line1, = ax.plot(x, y, 'r-') # Returns a tuple of line objects, thus the comma
 
-class RNet(nn.Module):
-
-    def __init__(self, m_hid, osc_hid):
-        super(RNet, self).__init__()
-        self.m_hid = m_hid
-        self.osc_hid = osc_hid
-
-        # Master
-        self.m_rnn = nn.RNNCell(5 + 2 * osc_hid, m_hid)
-        self.m_out = nn.Linear(m_hid, 2 * osc_hid)
-        self.m_s = Variable(torch.zeros(1, m_hid))
-
-        # Hip
-        self.h_rnn = nn.RNNCell(1 + 2 * osc_hid, osc_hid)
-        self.h_out = nn.Linear(osc_hid, 1)
-        self.h1_s = Variable(torch.zeros(1, osc_hid))
-        self.h2_s = Variable(torch.zeros(1, osc_hid))
-
-        # Knee
-        self.k_rnn = nn.RNNCell(1 + 2 * osc_hid, osc_hid)
-        self.k_out = nn.Linear(osc_hid, 1)
-        self.k1_s = Variable(torch.zeros(1, osc_hid))
-        self.k2_s = Variable(torch.zeros(1, osc_hid))
-
-        # Foot
-        self.f_rnn = nn.RNNCell(1 + osc_hid, osc_hid)
-        self.f_out = nn.Linear(osc_hid, 1)
-        self.f1_s = Variable(torch.zeros(1, osc_hid))
-        self.f2_s = Variable(torch.zeros(1, osc_hid))
-
-    def forward(self, x):
-        #h1, k1, f1, h2, k2, f2, *m_obs = x[0, [2, 3, 4, 5, 6, 7, 0, 1, 8, 9, 10]]
-
-        h1 = x[:, 2]
-        k1 = x[:, 3]
-        f1 = x[:, 4]
-        h2 = x[:, 5]
-        k2 = x[:, 6]
-        f2 = x[:, 7]
-        m_obs = x[:, [0, 1, 8, 9, 10]]
-
-        self.m_s = self.m_rnn(torch.cat([m_obs , self.h1_s , self.h2_s], 1), self.m_s)
-        out_m = self.m_out(self.m_s)
-
-        h1_s = self.h_rnn(torch.cat([h1.unsqueeze(0), out_m[:, :self.osc_hid], self.k1_s], 1), self.h1_s)
-        h2_s = self.h_rnn(torch.cat([h2.unsqueeze(0), out_m[:, self.osc_hid:], self.k2_s], 1), self.h2_s)
-
-        k1_s = self.k_rnn(torch.cat([k1.unsqueeze(0), self.h1_s, self.f1_s], 1), self.k1_s)
-        k2_s = self.k_rnn(torch.cat([k2.unsqueeze(0), self.h2_s, self.f2_s], 1), self.k2_s)
-
-        f1_s = self.f_rnn(torch.cat([f1.unsqueeze(0), self.f1_s], 1), self.f1_s)
-        f2_s = self.f_rnn(torch.cat([f2.unsqueeze(0), self.f2_s], 1), self.f2_s)
-
-        self.h1_s = h1_s
-        self.h2_s = h2_s
-        self.k1_s = k1_s
-        self.k2_s = k2_s
-        self.f1_s = f1_s
-        self.f2_s = f2_s
-
-        out_h1 = self.h_out(self.h1_s)
-        out_h2 = self.h_out(self.h2_s)
-
-        out_k1 = self.k_out(self.k1_s)
-        out_k2 = self.k_out(self.k2_s)
-
-        out_f1 = self.f_out(self.f1_s)
-        out_f2 = self.f_out(self.f2_s)
-
-        action = torch.cat([out_h1, out_k1, out_f1, out_h2, out_k2, out_f2], 1)
-
-        return action
-
-    def num_flat_features(self, x):
-        size = x.size()[1:]  # all dimensions except the batch dimension
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
-
-# add the model on top of the convolutional base
-model = RNet(4,2)
-model.apply(weights_init)
-
-
-
-def get_reward(weights, model, render=False):
-    cloned_model = copy.deepcopy(model)
-    for i, param in enumerate(cloned_model.parameters()):
-        try:
-            param.data.copy_(weights[i])
-        except:
-            param.data.copy_(weights[i].data)
-
-    env = gym.make("Walker2d-v2")
-    ob = env.reset()
-    done = False
-    total_reward = 0
-    while not done:
-        if render:
-            pass
-            #env.render()
-        batch = torch.from_numpy(ob[np.newaxis, ...]).float()
-        prediction = cloned_model(Variable(batch, volatile=True))
-        action = prediction.data[0]
-        ob, reward, done, _ = env.step(action)
-
-        total_reward += reward
-
-    #env.close()
-    return total_reward
-
-#
-# weights = pickle.load(open(os.path.abspath("es_walkerweights.p"), 'rb'))
-# print(get_reward(weights, model, render=True))
-# exit()
-
-partial_func = partial(get_reward, model=model)
-mother_parameters = list(model.parameters())
-
-es = EvolutionModule(
-    mother_parameters, partial_func, population_size=50,
-    sigma=0.1, learning_rate=0.001, reward_goal=300, consecutive_goal_stopping=20,
-    threadcount=3, cuda=False, render_test=True
-)
-start = time.time()
-final_weights = es.run(5000, print_step=1)
-end = time.time() - start
-
-pickle.dump(final_weights, open(os.path.abspath("es_walkerweights.p"), 'wb'))
-
-reward = partial_func(final_weights, render=True)
-
-print(f"Reward from final weights: {reward}")
-print(f"Time to completion: {end}")
-
+for phase in np.linspace(0, 10*np.pi, 500):
+    line1.set_ydata(np.sin(x + phase))
+    fig.canvas.draw()
+    fig.canvas.flush_events()
--- src/walker_IMIT_trainer.py
+++ src/walker_IMIT_trainer.py
@@ -1,18 +1,6 @@
-import argparse
-from collections import deque
-import copy
-from functools import partial
-import gc
 import logging
-from multiprocessing.pool import ThreadPool
-import os
 import pickle
-import random
-import sys
-import time
-
-# from evostra import EvolutionStrategy
-from pytorch_es import EvolutionModule
+import matplotlib.pyplot as plt
 from pytorch_es.utils.helpers import weights_init
 import gym
 from gym import logger as gym_logger
@@ -21,6 +9,7 @@ import torch
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
+import time
 
 gym_logger.setLevel(logging.CRITICAL)
 
@@ -313,6 +302,243 @@ class RNet2(nn.Module):
         return num_features
 
 
+class RNet3(nn.Module):
+    def __init__(self, m_hid, osc_hid):
+        super(RNet3, self).__init__()
+        self.m_hid = m_hid
+        self.osc_hid = osc_hid
+
+        # Set states
+        self.reset()
+
+        # Master
+        self.m_rnn = nn.GRUCell(5 + 2 * osc_hid, m_hid)
+        self.m_out = nn.Linear(m_hid, 2 * osc_hid)
+
+        # Hip
+        self.h_rnn1 = nn.GRUCell(1 + 2 * osc_hid, osc_hid)
+        self.h_rnn2 = nn.GRUCell(1 + 2 * osc_hid, osc_hid)
+        self.h_out1 = nn.Linear(osc_hid, 1)
+        self.h_out2 = nn.Linear(osc_hid, 1)
+        self.h_out1_down = nn.Linear(osc_hid, osc_hid)
+        self.h_out2_down = nn.Linear(osc_hid, osc_hid)
+        self.h_out1_up = nn.Linear(osc_hid, osc_hid)
+        self.h_out2_up = nn.Linear(osc_hid, osc_hid)
+
+        # Knee
+        self.k_rnn1 = nn.GRUCell(1 + 2 * osc_hid, osc_hid)
+        self.k_rnn2 = nn.GRUCell(1 + 2 * osc_hid, osc_hid)
+        self.k_out1 = nn.Linear(osc_hid, 1)
+        self.k_out2 = nn.Linear(osc_hid, 1)
+        self.k_out1_down = nn.Linear(osc_hid, osc_hid)
+        self.k_out2_down = nn.Linear(osc_hid, osc_hid)
+        self.k_out1_up = nn.Linear(osc_hid, osc_hid)
+        self.k_out2_up = nn.Linear(osc_hid, osc_hid)
+
+        # Foot
+        self.f_rnn1 = nn.GRUCell(1 + osc_hid, osc_hid)
+        self.f_rnn2 = nn.GRUCell(1 + osc_hid, osc_hid)
+        self.f_out1 = nn.Linear(osc_hid, 1)
+        self.f_out2 = nn.Linear(osc_hid, 1)
+        self.f_out1_up = nn.Linear(osc_hid, osc_hid)
+        self.f_out2_up = nn.Linear(osc_hid, osc_hid)
+
+    def normalize(self, tensor):
+        tensor_norm = torch.norm(tensor, p=2, dim=1).detach()
+        return tensor.div(tensor_norm.expand_as(tensor))
+
+    def identity(self, tensor):
+        return tensor
+
+    def forward(self, x):
+        #h1, k1, f1, h2, k2, f2, *m_obs = x[0, [2, 3, 4, 5, 6, 7, 0, 1, 8, 9, 10]]
+
+        h1 = x[:, 2]
+        k1 = x[:, 3]
+        f1 = x[:, 4]
+        h2 = x[:, 5]
+        k2 = x[:, 6]
+        f2 = x[:, 7]
+        m_obs = x[:, [0, 1, 8, 9, 10]]
+
+        self.m_s = self.m_rnn(self.identity(torch.cat([m_obs , self.h1_up , self.h2_up], 1)), self.m_s)
+        out_m = self.m_out(self.m_s)
+
+        h1_s = self.h_rnn1(self.identity(torch.cat([h1.unsqueeze(0), out_m[:, :self.osc_hid], self.k1_up], 1)), self.h1_s)
+        h2_s = self.h_rnn2(self.identity(torch.cat([h2.unsqueeze(0), out_m[:, self.osc_hid:], self.k2_up], 1)), self.h2_s)
+
+        k1_s = self.k_rnn1(self.identity(torch.cat([k1.unsqueeze(0), self.h1_down, self.f1_up], 1)), self.k1_s)
+        k2_s = self.k_rnn2(self.identity(torch.cat([k2.unsqueeze(0), self.h2_down, self.f2_up], 1)), self.k2_s)
+
+        f1_s = self.f_rnn1(self.identity(torch.cat([f1.unsqueeze(0), self.k1_down], 1)), self.f1_s)
+        f2_s = self.f_rnn2(self.identity(torch.cat([f2.unsqueeze(0), self.k2_down], 1)), self.f2_s)
+
+        self.h1_s = self.identity(h1_s)
+        self.h2_s = self.identity(h2_s)
+        self.k1_s = self.identity(k1_s)
+        self.k2_s = self.identity(k2_s)
+        self.f1_s = self.identity(f1_s)
+        self.f2_s = self.identity(f2_s)
+
+        out_h1 = self.h_out1(self.h1_s)
+        out_h2 = self.h_out2(self.h2_s)
+        h1_down = self.h_out1_down(self.h1_s)
+        h1_up = self.h_out1_up(self.h1_s)
+        h2_down = self.h_out2_down(self.h2_s)
+        h2_up = self.h_out2_up(self.h2_s)
+
+        out_k1 = self.k_out1(self.k1_s)
+        out_k2 = self.k_out2(self.k2_s)
+        k1_down = self.k_out1_down(self.k1_s)
+        k1_up = self.k_out1_up(self.k1_s)
+        k2_down = self.k_out2_down(self.k2_s)
+        k2_up = self.k_out2_up(self.k2_s)
+
+        out_f1 = self.f_out1(self.f1_s)
+        out_f2 = self.f_out2(self.f2_s)
+        f1_up = self.f_out1_up(self.f1_s)
+        f2_up = self.f_out2_up(self.f2_s)
+
+        self.h1_down = h1_down
+        self.h2_down = h2_down
+        self.h1_up = h1_up
+        self.h2_up = h2_up
+
+        self.k1_down = k1_down
+        self.k2_down = k2_down
+        self.k1_up = k1_up
+        self.k2_up = k2_up
+
+        self.f1_up = f1_up
+        self.f2_up = f2_up
+
+        action = torch.cat([out_h1, out_k1, out_f1, out_h2, out_k2, out_f2], 1)
+
+        #print([self.m_s.data, self.h1_s.data, self.h2_s.data, self.k1_s.data, self.k2_s.data, self.f1_s.data, self.f2_s.data])
+
+        return action
+
+    def reset(self):
+        # Master
+        self.m_s = Variable(torch.zeros(1, self.m_hid)).float()
+
+        # Hip
+        self.h1_s = Variable(torch.zeros(1, self.osc_hid)).float()
+        self.h1_up = Variable(torch.zeros(1, self.osc_hid)).float()
+        self.h1_down = Variable(torch.zeros(1, self.osc_hid)).float()
+
+        self.h2_s = Variable(torch.zeros(1, self.osc_hid)).float()
+        self.h2_up = Variable(torch.zeros(1, self.osc_hid)).float()
+        self.h2_down = Variable(torch.zeros(1, self.osc_hid)).float()
+
+        # Knee
+        self.k1_s = Variable(torch.zeros(1, self.osc_hid)).float()
+        self.k1_up = Variable(torch.zeros(1, self.osc_hid)).float()
+        self.k1_down = Variable(torch.zeros(1, self.osc_hid)).float()
+
+        self.k2_s = Variable(torch.zeros(1, self.osc_hid)).float()
+        self.k2_up = Variable(torch.zeros(1, self.osc_hid)).float()
+        self.k2_down = Variable(torch.zeros(1, self.osc_hid)).float()
+
+        # Foot
+        self.f1_s = Variable(torch.zeros(1, self.osc_hid)).float()
+        self.f1_up = Variable(torch.zeros(1, self.osc_hid)).float()
+
+        self.f2_s = Variable(torch.zeros(1, self.osc_hid)).float()
+        self.f2_up = Variable(torch.zeros(1, self.osc_hid)).float()
+
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+
+class PNet(nn.Module):
+
+    def __init__(self):
+        super(PNet, self).__init__()
+
+        self.f_up_l1 = nn.Linear(2, 2)
+        self.f_up_l2 = nn.Linear(2, 2)
+        self.k_up_l1 = nn.Linear(4, 4)
+        self.k_up_l2 = nn.Linear(4, 4)
+        self.h_up_l1 = nn.Linear(6, 4)
+        self.h_up_l2 = nn.Linear(4, 4)
+
+        self.m_down_l1 = nn.Linear(13, 8)
+        self.m_down_l2_1 = nn.Linear(8, 4)
+        self.m_down_l2_2 = nn.Linear(8, 4)
+        self.k_down_l1 = nn.Linear(4, 4)
+        self.k_down_l2 = nn.Linear(4, 4)
+        self.h_down_l1 = nn.Linear(4, 4)
+        self.h_down_l2 = nn.Linear(4, 4)
+
+        self.h_act_l1 = nn.Linear(4, 2)
+        self.h_act_l2 = nn.Linear(2, 1)
+        self.k_act_l1 = nn.Linear(4, 2)
+        self.k_act_l2 = nn.Linear(2, 1)
+        self.f_act_l1 = nn.Linear(4, 2)
+        self.f_act_l2 = nn.Linear(2, 1)
+
+    def forward(self, x):
+
+        h1 = x[:, 2].unsqueeze(1)
+        k1 = x[:, 3].unsqueeze(1)
+        f1 = x[:, 4].unsqueeze(1)
+        h2 = x[:, 5].unsqueeze(1)
+        k2 = x[:, 6].unsqueeze(1)
+        f2 = x[:, 7].unsqueeze(1)
+        h1d = x[:, 11].unsqueeze(1)
+        k1d = x[:, 12].unsqueeze(1)
+        f1d = x[:, 13].unsqueeze(1)
+        h2d = x[:, 14].unsqueeze(1)
+        k2d = x[:, 15].unsqueeze(1)
+        f2d = x[:, 16].unsqueeze(1)
+        m_obs = x[:, [0, 1, 8, 9, 10]]
+
+        f_up_1 = F.relu(self.f_up_l2(F.relu(self.f_up_l1(torch.cat([f1, f1d], 1)))))
+        k_up_1 = F.relu(self.k_up_l2(F.relu(self.k_up_l1(torch.cat([k1, k1d, f_up_1], 1)))))
+        h_up_1 = F.relu(self.h_up_l2(F.relu(self.h_up_l1(torch.cat([h1, h1d, k_up_1], 1)))))
+
+        f_up_2 = F.relu(self.f_up_l2(F.relu(self.f_up_l1(torch.cat([f2, f2d], 1)))))
+        k_up_2 = F.relu(self.k_up_l2(F.relu(self.k_up_l1(torch.cat([k2, k2d, f_up_2], 1)))))
+        h_up_2 = F.relu(self.h_up_l2(F.relu(self.h_up_l1(torch.cat([h2, h2d, k_up_2], 1)))))
+
+        m_down = F.relu(self.m_down_l1(torch.cat([m_obs, h_up_1, h_up_2], 1)))
+        m_down_1 = F.relu(self.m_down_l2_1(m_down))
+        m_down_2 = F.relu(self.m_down_l2_2(m_down))
+
+        h_down_1 = F.relu(self.h_down_l2(F.tanh(self.h_down_l1(m_down_1))))
+        k_down_1 = F.relu(self.k_down_l2(F.tanh(self.k_down_l1(h_down_1))))
+
+        h_down_2 = F.relu(self.h_down_l2(F.tanh(self.h_down_l1(m_down_2))))
+        k_down_2 = F.relu(self.k_down_l2(F.tanh(self.k_down_l1(h_down_2))))
+
+        h_act_1 = self.h_act_l2(F.relu(self.h_act_l1(m_down_1)))
+        k_act_1 = self.k_act_l2(F.relu(self.k_act_l1(h_down_1)))
+        f_act_1 = self.f_act_l2(F.relu(self.f_act_l1(k_down_1)))
+
+        h_act_2 = self.h_act_l2(F.relu(self.h_act_l1(m_down_2)))
+        k_act_2 = self.k_act_l2(F.relu(self.k_act_l1(h_down_2)))
+        f_act_2 = self.f_act_l2(F.relu(self.f_act_l1(k_down_2)))
+
+        act = torch.cat([h_act_1, f_act_1, k_act_1, h_act_2, k_act_2, f_act_2], 1)
+
+        return act
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+    def reset(self):
+        pass
+
 def evaluate(model, env, iters):
     print("Starting visual evaluation")
     for i in range(iters):
@@ -341,27 +567,36 @@ def train_imitation(model,baseline, trajectories, iters):
     print("Starting training. Obs dim: {}, Act dim: {}".format(obs_dim, act_dim))
 
     lossfun = nn.MSELoss()
-    rnn_optim = torch.optim.Adam(model.parameters(), lr=1e-2)
-    baseline_optim = torch.optim.Adam(baseline.parameters(), lr=5e-3)
+    rnn_optim = torch.optim.Adam(model.parameters(), lr=3e-3)
+    baseline_optim = torch.optim.Adam(baseline.parameters(), lr=3e-3)
+
+    maxlen = 500
 
     for i in range(iters):
         # Sample random whole episodes
         rand_episode = trajectories[np.random.randint(0, N)]
+
+        # if len(rand_episode) > maxlen:
+        #     N = maxlen
+        # else:
+        #     N = len(rand_episode)
+
         obs_list, act_list = zip(*rand_episode)
         obs_array = torch.from_numpy(np.array(obs_list, dtype=np.float32))
         act_array = torch.from_numpy(np.array(act_list, dtype=np.float32))
 
-        rnn_pred_list = []
+        #rnn_pred_list = []
 
         # Rollout
         model.reset()
 
         rnn_optim.zero_grad()
         baseline_optim.zero_grad()
-        for obs in obs_array:
-            rnn_pred_list.append(model(obs.unsqueeze(0)))
+        # for obs in obs_array:
+        #     rnn_pred_list.append(model(obs.unsqueeze(0)))
 
-        rnn_preds = torch.cat(rnn_pred_list, 0)
+        #rnn_preds = torch.cat(rnn_pred_list, 0)
+        rnn_preds = model(obs_array)
         baseline_preds = baseline(obs_array)
 
         # MSE & gradients
@@ -379,32 +614,35 @@ def train_imitation(model,baseline, trajectories, iters):
     torch.save(baseline, 'baseline_imit.pt')
     torch.save(model, 'rnn_imit.pt')
 
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
 # Baseline model
 baseline = Baseline(17, 6)
 
 # RNN
-model = RNet2(4,2)
-model.apply(weights_init)
+model = PNet()
 
 # Load trajectories
-trajectories = pickle.load(open("/home/silverjoda/SW/baselines/data/Walker2d-v2_rollouts_1", 'rb'))
+trajectories = pickle.load(open("/home/silverjoda/SW/baselines/data/Walker2d-v2_rollouts_2", 'rb'))
 
 # TODO: VISUALIZE RNN HIDDEN STATE VALUES AND FIX HIDDEN STATE BLOW UP IF NECESSARY
 # TODO: PERFORM ROLLOUTS OF TRAINED BASELINES AND TRAINED RNN MODELS TO SEE HOW THE RNN DOES
-# TODO: CHECK DEFAULT TRUNCATION FACTOR IN GRU CELL
+
+print("Model params: {}, baseline params: {}".format(count_parameters(model), count_parameters(baseline)))
 
 try:
     # Train model to imitate trajectories
-    train_imitation(model, baseline, trajectories, 0)
+    train_imitation(model, baseline, trajectories, 2000)
 except KeyboardInterrupt:
     print("User interrupted process.")
 
 env = gym.make("Walker2d-v2")
 
-#print("Evaluating baseline")
-#baseline = torch.load('baseline_imit.pt')
-#evaluate(baseline, env, 5)
-#time.sleep(1)
-print("Evaluating RNN")
-model = torch.load('rnn_imit.pt')
-evaluate(model, env, 1000)
+print("Evaluating baseline")
+baseline = torch.load('baseline_imit.pt')
+evaluate(baseline, env, 10)
+time.sleep(1)
+print("Evaluating Model")
+#model = torch.load('rnn_imit.pt')
+evaluate(model, env, 10)
