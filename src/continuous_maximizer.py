c3d765d5938ac5ec09248d065af2ec41c93cb445
silverjoda <heizen.sh@gmail.com>
2018-09-20 17:00:32 +0200

    Initcom

--- src/continuous_maximizer.py
+++ src/continuous_maximizer.py
@@ -17,14 +17,17 @@ class Model(nn.Module):
         self.reset()
 
         self.rnn_out = nn.GRUCell(obs_dim + act_dim, n_hid)
-        self.l1 = nn.Linear(n_hid, 16)
+        self.l1a = nn.Linear(n_hid, 16)
+        self.l1b = nn.Linear(n_hid, 16)
         self.out = nn.Linear(16, obs_dim)
+        self.rew = nn.Linear(16, obs_dim)
 
 
     def forward(self, x):
         self.h = self.rnn(x, self.h)
-        l1 = self.l1(self.h)
-        return self.m_out(l1)
+        l1a = self.l1a(self.h)
+        l1b = self.l1b(self.h)
+        return self.out(l1a), self.rew(l1b)
 
 
     def reset(self, batchsize=1):
@@ -71,13 +74,12 @@ def main():
     policy = Policy(obs_dim, act_dim, obs_dim)
 
     # Train prediction model on random rollouts
-    loss_fun = torch.nn.MSELoss()
-    optim = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-4)
+    loss_fun_model = torch.nn.MSELoss()
+    optim_model = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-4)
     model_eps = 1000
     for i in range(model_eps):
-        optim.zero_grad()
-
         s = env.reset()
+        model.reset()
         done = False
 
         states = []
@@ -99,19 +101,69 @@ def main():
         pred_tens = torch.stack(predictions)
 
         # Calculate loss
-        loss = loss_fun(pred_tens, newstates_tens)
+        loss = loss_fun_model(pred_tens, newstates_tens)
 
         # Backprop
+        optim_model.zero_grad()
         loss.backward()
 
         # Update
-        optim.step()
+        optim_model.step()
 
-        model.reset(1)
 
     # Training algorithm:
+    optim_policy = torch.optim.Adam(policy.parameters(), lr=1e-3, weight_decay=1e-4)
+    states = []
+    state_predictions = []
+    rewards = []
+    trn_eps = 1000
+    for i in range(trn_eps):
 
-    pass
+        done = False
+        s = env.reset()
+        model.reset()
+        policy.reset()
+
+        sdiff = torch.zeros(1, obs_dim)
+        pred_state = torch.from_numpy(s).unsqueeze(0)
+
+        while not done:
+            states.append(s)
+
+            # Predict action from current state
+            pred_a = policy(pred_state - sdiff)
+
+            # Predict next state from current state and predicted action
+            pred_s, pred_rew = model(torch.cat([torch.from_numpy(s).unsqueeze(0), pred_a]))
+            state_predictions.append(pred_s)
+
+            # Perform simulation to get real state
+            s = env.step(pred_a.numpy())
+
+            # Difference between predicted state and real
+            sdiff = pred_s - torch.from_numpy(s)
+
+        newstates = np.asarray(states[1:], dtype=np.float32)
+        rewards = np.asarray(rewards, dtype=np.float32)
+
+        # Convert to torch tensors
+        newstates_tens = torch.from_numpy(newstates)
+        rewards_tens = torch.from_numpy(rewards)
+        pred_tens = torch.stack(state_predictions)
+
+        # Calculate loss
+        model_loss = loss_fun_model(pred_tens, newstates_tens)
+        policy_loss = -rewards_tens.sum() # Just x coordinate
+
+        # Backprop
+        optim_model.zero_grad()
+        optim_policy.zero_grad()
+        model_loss.backward()
+        policy_loss.backward()
+
+        # Update
+        optim_model.step()
+        optim_policy.step()
 
 if __name__=='__main__':
     main()
