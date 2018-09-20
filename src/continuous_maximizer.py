098cc45f8bcc14f138a64bf95c15ef012840db03
silverjoda <heizen.sh@gmail.com>
2018-09-20 17:57:57 +0200

    Initcom

--- src/continuous_maximizer.py
+++ src/continuous_maximizer.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.autograd import Variable
 import gym
 import time
@@ -17,21 +18,21 @@ class Model(nn.Module):
         self.reset()
 
         self.rnn_out = nn.GRUCell(obs_dim + act_dim, n_hid)
-        self.l1a = nn.Linear(n_hid, 16)
+        self.l1a = nn.Linear(n_hid, 32)
         self.l1b = nn.Linear(n_hid, 16)
-        self.out = nn.Linear(16, obs_dim)
+        self.out = nn.Linear(32, obs_dim)
         self.rew = nn.Linear(16, obs_dim)
 
 
     def forward(self, x):
         self.h = self.rnn(x, self.h)
-        l1a = self.l1a(self.h)
-        l1b = self.l1b(self.h)
+        l1a = F.relu(self.l1a(self.h))
+        l1b = F.relu(self.l1b(self.h))
         return self.out(l1a), self.rew(l1b)
 
 
     def reset(self, batchsize=1):
-        self.h = Variable(torch.zeros(batchsize, self.n_hid)).float()
+        self.h = torch.zeros(batchsize, self.n_hid).float()
 
 
 class Policy(nn.Module):
@@ -46,26 +47,26 @@ class Policy(nn.Module):
         self.reset()
 
         self.rnn_out = nn.GRUCell(obs_dim, n_hid)
-        self.l1 = nn.Linear(n_hid, 16)
-        self.out = nn.Linear(16, act_dim)
+        self.l1 = nn.Linear(n_hid, 32)
+        self.out = nn.Linear(32, act_dim)
 
 
     def forward(self, x):
         self.h = self.rnn(x, self.h)
-        l1 = self.l1(self.h)
-        return self.m_out(l1)
+        l1 = F.relu(self.l1(self.h))
+        return self.out(l1)
 
 
     def reset(self, batchsize=1):
-        self.h = Variable(torch.zeros(batchsize, self.n_hid)).float()
+        self.h = torch.zeros(batchsize, self.n_hid).float()
 
 
 def main():
 
     # Create environment
     env = gym.make("Hopper-v2")
-    obs_dim = env.observation_space
-    act_dim = env.action_space
+    obs_dim = env.observation_space.shape[0]
+    act_dim = env.action_space.shape[0]
 
     # Create prediction model
     model = Model(obs_dim, act_dim, obs_dim)
@@ -74,49 +75,57 @@ def main():
     policy = Policy(obs_dim, act_dim, obs_dim)
 
     # Train prediction model on random rollouts
-    loss_fun_model = torch.nn.MSELoss()
+    MSE = torch.nn.MSELoss()
     optim_model = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-4)
-    model_eps = 1000
+    model_eps = 10
     for i in range(model_eps):
         s = env.reset()
         model.reset()
         done = False
 
         states = []
-        predictions = []
+        rewards = []
+        state_predictions = []
+        reward_predictions = []
         while not done:
             a = env.action_space.sample()
             states.append(s)
 
             # Make prediction
-            pred = model(torch.cat([np.expand_dims(np.asarray(s),0), np.expand_dims(np.asarray(a),0)], 1))
-            predictions.append(pred)
+            pred, rew = model(torch.cat([np.expand_dims(np.asarray(s),0), np.expand_dims(np.asarray(a),0)], 1))
+            state_predictions.append(pred)
+            reward_predictions.append(rew)
 
             s, rew, done, info = env.step(a)
-
-        newstates = np.asarray(states[1:], dtype=np.float32)
+            rewards.append(rew)
 
         # Convert to torch tensors
-        newstates_tens = torch.from_numpy(newstates)
-        pred_tens = torch.stack(predictions)
+        newstates_tens = torch.from_numpy(np.asarray(states[1:], dtype=np.float32))
+        rewards_tens = torch.from_numpy(np.asarray(rewards, dtype=np.float32))
+        state_pred_tens = torch.stack(state_predictions)
+        rew_pred_tens = torch.stack(reward_predictions)
 
         # Calculate loss
-        loss = loss_fun_model(pred_tens, newstates_tens)
+        loss_states = MSE(state_pred_tens, newstates_tens)
+        loss_rewards = MSE(rew_pred_tens, rewards_tens)
+        total_loss = loss_rewards + loss_states
 
         # Backprop
         optim_model.zero_grad()
-        loss.backward()
+        total_loss.backward()
 
         # Update
         optim_model.step()
+        print("Iter: {}/{}, states_loss: {}, rewards_loss: {}".format(i, model_eps, loss_states, loss_rewards))
 
+    print("Finished training model")
 
     # Training algorithm:
     optim_policy = torch.optim.Adam(policy.parameters(), lr=1e-3, weight_decay=1e-4)
     states = []
     state_predictions = []
     rewards = []
-    trn_eps = 1000
+    trn_eps = 10
     for i in range(trn_eps):
 
         done = False
