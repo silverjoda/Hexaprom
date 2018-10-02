b654628ed147fe95d1f458c0e71a0d47427e4f39
silverjoda <heizen.sh@gmail.com>
2018-10-02 14:00:56 +0200

    Initcom

--- src/chained_EXP.py
+++ src/chained_EXP.py
@@ -1,10 +1,7 @@
 import numpy as np
 import gym
 import cma
-np.random.seed(0)
-
 from weight_distributor import Wdist
-np.random.seed(0)
 from time import sleep
 import quaternion
 
@@ -20,7 +17,10 @@ def f(w):
 
     states = [0] * N
 
-    print(goal_dir)
+    joint_activation_list = []
+    master_activation_list = []
+
+    #print(goal_dir)
     while not done:
 
         # Observations
@@ -32,8 +32,10 @@ def f(w):
         newstates = []
 
         obs = list(env_obs[2:3]) + states[0:1]
-        l1 = np.tanh(np.matmul(obs, wdist.get_w('w_m1', w)) + wdist.get_w('b_m1', w))
-        mout = np.tanh(np.matmul(l1, wdist.get_w('w_m2', w)) + wdist.get_w('b_m2', w))
+        l1_m = np.tanh(np.matmul(obs, wdist.get_w('w_m1', w)) + wdist.get_w('b_m1', w))
+        mout = np.tanh(np.matmul(l1_m, wdist.get_w('w_m2', w)) + wdist.get_w('b_m2', w))
+
+        master_activation_list.append((l1_m, mout))
 
         for i in range(N):
             if i == 0:
@@ -42,11 +44,13 @@ def f(w):
                 nt = states[N-1:N] + [0]
             else:
                 nt = states[i-1:i] + states[i+1:i+2]
-            obs = list(env_obs[i+2:i+3]) + nt
+            obs = list(env_obs[i+3:i+4]) + nt
             l1 = np.tanh(np.matmul(obs, wdist.get_w('w_l1', w)) + wdist.get_w('b_l1', w))
             t, s = np.tanh(np.matmul(l1, wdist.get_w('w_l2', w)) + wdist.get_w('b_l2', w))
             torques.append(t)
             newstates.append(s)
+
+            joint_activation_list.append((l1,t,s))
 
         states = newstates
         # -------------
@@ -66,6 +70,19 @@ def f(w):
             env.render()
 
         reward += reward_dir
+
+    l1_list, t_list, s_list = zip(*joint_activation_list)
+    l1_mean, l1_std = np.mean(l1_list), np.std(l1_list)
+    t_mean, t_std = np.mean(t_list), np.std(t_list)
+    s_mean, s_std = np.mean(s_list), np.std(s_list)
+
+    l1_m_list, mout_list = zip(*master_activation_list)
+    mout_mean, mout_std = np.mean(mout_list), np.std(mout_list)
+    l1_m_mean, l1_m_std = np.mean(l1_m_list), np.std(l1_m_list)
+
+    # Print run statistics
+    print("w_mean: {}, w_std: {}, w_min: {}, w_max: {}, m_l1_mean: {}, m_l1_std: {}, m_out_mean: {}, m_out_std: {}, l1_mean: {}, l1_std: {}, s_mean: {}, s_std: {}, t_mean: {}, t_std: {}".
+          format(np.mean(w), np.std(w), np.min(w), np.max(w), l1_m_mean, l1_m_std, mout_mean, mout_std, l1_mean, l1_std, s_mean, s_std, t_mean, t_std))
 
     return -reward
 
@@ -102,8 +119,7 @@ w = np.random.randn(N_weights) * 0.5
 
 es = cma.CMAEvolutionStrategy(w, 0.5)
 
-# TODO: Reminder: The state space was wrongly defined previously. Try with new state space
-
+# TODO: Plot all intermediate values and activations
 try:
     es.optimize(f, iterations=2000)
 except KeyboardInterrupt:
