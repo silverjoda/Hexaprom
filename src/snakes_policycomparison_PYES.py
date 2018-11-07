8424b873f835b97029759e274bb317a40a1e2440
silverjoda <heizen.sh@gmail.com>
2018-11-07 19:07:08 +0100

    Initcom

--- src/snakes_policycomparison_PYES.py
+++ src/snakes_policycomparison_PYES.py
@@ -3,7 +3,7 @@ import gym
 import cma
 from time import sleep
 import quaternion
-import torch
+import torch as T
 import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_ES
@@ -16,7 +16,7 @@ class Baseline(nn.Module):
         self.fc1 = nn.Linear(18, 7)
 
     def forward(self, x):
-        x = F.tanh(self.fc1(x))
+        x = nn.tanh(self.fc1(x))
         return x
 
 
@@ -24,34 +24,33 @@ class ConvPolicy(nn.Module):
     def __init__(self):
         super(ConvPolicy, self).__init__()
 
-        self.fc_obs = nn.Linear(4, 2)
-        self.fc_emb = nn.Linear(2, 2)
+        self.fc_emb = nn.Linear(6, 2)
 
         self.conv1 = nn.Conv1d(in_channels=2, out_channels=1, kernel_size=3, stride=2)
         self.conv2 = nn.Conv1d(in_channels=1, out_channels=1, kernel_size=2, stride=1)
 
         self.deconv1 = nn.ConvTranspose1d(in_channels=1, out_channels=1, kernel_size=2, stride=1)
         self.deconv2 = nn.ConvTranspose1d(in_channels=1, out_channels=1, kernel_size=3, stride=2)
+        self.deconv3 = nn.ConvTranspose1d(in_channels=2, out_channels=1, kernel_size=1, stride=1)
 
     def forward(self, x):
         obs = x[:, :4]
         j = x[:, 4:11]
         jd = x[:, 11:]
-        jcat = torch.cat([j,jd], 1).unsqueeze(1) # Concatenate j and jd so that they are 2 parallel channels
+        jcat = T.cat([j.unsqueeze(1) ,jd.unsqueeze(1)], 1) # Concatenate j and jd so that they are 2 parallel channels
 
-        fc_obs = F.relu(self.fc_obs(obs))
-
-        fm = F.relu(self.conv1(jcat))
-        fm = F.relu(self.conv2(fm))
+        fm = T.tanh(self.conv1(jcat))
+        fm = T.tanh(self.conv2(fm))
         fm = fm.squeeze(1)
 
-        fc_emb = F.relu(self.fc_emb(fc_obs + fm))
+        fc_emb = T.tanh(self.fc_emb(T.cat((obs, fm), 1)))
         fc_emb = fc_emb.unsqueeze(1)
 
-        fm = F.relu(self.deconv1(fc_emb))
+        fm = T.tanh(self.deconv1(fc_emb))
         fm = self.deconv2(fm)
+        fm = self.deconv3(T.cat((fm, j.unsqueeze(1)), 1))
 
-        x = fm
+        x = fm.unsqueeze(1)
 
         return x
 
@@ -66,23 +65,23 @@ class SymPolicy(nn.Module):
 
 
     def forward(self, x):
-        obs = x[:, :5]
-        j = x[:, 5:12]
-        jd = x[:, 12:]
-        jcat = torch.cat([j,jd], 1).unsqueeze(1) # Concatenate j and jd so that they are 2 parallel channels
+        obs = x[:, :4]
+        j = x[:, 4:11]
+        jd = x[:, 11:]
+        jcat = T.cat([j.unsqueeze(1), jd.unsqueeze(1)], 1)  # Concatenate j and jd so that they are 2 parallel channels
 
-        j_in = torch.zeros((1,2))
+        j_in = T.zeros((1,2)).double()
         for i in range(7):
-            j_in += self.fc_j1(jcat[:, i])
+            j_in += self.fc_ji(jcat[:, :, i])
 
-        j_in = F.relu(j_in)
-        emb = F.relu(self.fc_e(torch.cat([obs, j_in], 1)))
+        j_in = T.tanh(j_in)
+        emb = T.tanh(self.fc_e(T.cat([obs, j_in], 1)))
 
         j_out = []
         for i in range(7):
-            j_out.append(self.fc_jo(emb + jcat[i]))
+            j_out.append(self.fc_jo(emb + jcat[:, :, i]))
 
-        j_out = torch.stack(j_out, 1)
+        j_out = T.cat(j_out, 1)
 
         return j_out
 
@@ -97,26 +96,26 @@ class RecPolicy(nn.Module):
         self.fc_out = nn.Linear(2, 1)
 
     def forward(self, x):
-        obs = x[:, :5]
-        j = x[:, 5:12]
-        jd = x[:, 12:]
-        jcat = torch.cat([j,jd], 1).unsqueeze(1) # Concatenate j and jd so that they are 2 parallel channels
+        obs = x[:, :4]
+        j = x[:, 4:11]
+        jd = x[:, 11:]
+        jcat = T.cat([j.unsqueeze(1), jd.unsqueeze(1)], 1)  # Concatenate j and jd so that they are 2 parallel channels
 
-        h = torch.zeros(1, 2).float()
+        h = T.zeros(1, 2).double()
 
         h_up = []
-        for i in range(7):
-            h = self.rnn(jcat[i], h)
+        for i in reversed(range(7)):
+            h = self.r_up(jcat[:, :, i], h)
             h_up.append(h)
 
-        h = self.fc_obs(torch.cat((obs, h), 1))
+        h = self.fc_obs(T.cat((obs, h), 1))
 
         acts = []
         for i in range(7):
-            h = self.rnn(h_up[i], h)
+            h = self.r_down(h_up[i], h)
             acts.append(self.fc_out(h))
 
-        return torch.stack(acts, 1)
+        return T.cat(acts, 1)
 
 
 class AggregPolicy(nn.Module):
@@ -131,28 +130,28 @@ class AggregPolicy(nn.Module):
 
 
     def forward(self, x):
-        obs = x[:, :5]
-        j = x[:, 5:12]
-        jd = x[:, 12:]
-        jcat = torch.cat([j,jd], 1).unsqueeze(1) # Concatenate j and jd so that they are 2 parallel channels
+        obs = x[:, :4]
+        j = x[:, 4:11]
+        jd = x[:, 11:]
+        jcat = T.cat([j.unsqueeze(1), jd.unsqueeze(1)], 1)  # Concatenate j and jd so that they are 2 parallel channels
 
-        h_j_list = [self.fc_init(jcat[:, i]) for i in range(7)]
+        h_j_list = [self.fc_j_init(jcat[:, i]) for i in range(7)]
         h_m = self.fc_m_init(obs)
 
         # 7 iterations
         for _ in range(7):
             h_m_new = self.rnn_m(h_j_list[0], h_m)
 
-            h_j_new_list = [self.rnn_j(torch.cat((h_m, h_j_list[1]), 1), h_j_list[0])]
+            h_j_new_list = [self.rnn_j(T.cat((h_m, h_j_list[1]), 1), h_j_list[0])]
             # Go over each non-master joint
             for i in range(1,6):
-                h_j_new_list.append(self.rnn_j(torch.cat((h_j_list[i-1], h_j_list[i+1]), 1), h_j_list[i]))
+                h_j_new_list.append(self.rnn_j(T.cat((h_j_list[i-1], h_j_list[i+1]), 1), h_j_list[i]))
             h_j_new_list.append(self.rnn_j(h_j_list[5], h_j_list[-1]))
 
             h_m = h_m_new
             h_j_list = h_j_new_list
 
-        acts = torch.stack([self.fc_act(h for h in h_j_list)], 1)
+        acts = T.stack([self.fc_act(h for h in h_j_list)], 1)
 
         return acts
 
@@ -164,7 +163,7 @@ def f_wrapper(env, policy, animate):
         obs = env.reset()
 
         # Inject current parameters into policy ## CONSIDER MAKING HARD COPY OF POLICY HERE NOT TO INTERFERE WITH INITIAL POLICY ##
-        pytorch_ES.vector_to_parameters(torch.from_numpy(w), policy.parameters())
+        pytorch_ES.vector_to_parameters(T.from_numpy(w), policy.parameters())
 
         while not done:
 
@@ -172,8 +171,8 @@ def f_wrapper(env, policy, animate):
             obs = np.concatenate((obs[0:1],obs[8:11],obs[1:8],obs[11:]))
 
             # Get action from policy
-            with torch.no_grad():
-                act = policy(torch.from_numpy(np.expand_dims(obs, 0)))[0].numpy()
+            with T.no_grad():
+                act = policy(T.from_numpy(np.expand_dims(obs, 0)))[0].numpy()
 
             # Step environment
             obs, rew, done, _ = env.step(act)
@@ -232,11 +231,12 @@ def train(params):
     return -es.result.fbest
 
 env_name = "SwimmerLong-v0"
-policyfunctions = [Baseline, ConvPolicy, SymPolicy, RecPolicy, AggregPolicy]
+#policyfunctions = [Baseline, ConvPolicy, SymPolicy, RecPolicy, AggregPolicy]
+policyfunctions = [AggregPolicy]
 
 for p in policyfunctions:
     print("Training with {} policy.".format(p.__name__))
-    fbest = train((env_name, p, 1000, False))
+    fbest = train((env_name, p, 100, True))
     print("Policy {} max score: {}".format(p.__name__, fbest))
 
 print("Done, exiting.")
